32404a83d453c26890d095b62170d45c2a125335
Daniel Schäfer <ds@struckmeierfliesen.de>
2017-05-24 10:44:52 +0200

    Add NotYetImplementedException TODO: Should be thrown in every not implemented method

---
+++ src/NotYetImplementedException.java
@@ -0,0 +1,6 @@
+/**
+ * Throw if a method has not yet been implemented.
+ * These implementations have to be added in the near future for the program to work!
+ */
+public class NotYetImplementedException extends RuntimeException{
+}
