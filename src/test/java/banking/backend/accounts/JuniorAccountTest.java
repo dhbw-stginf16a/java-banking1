4308e863516d61a6244b517702d6dfc0c28150f7
guserav <erik.zeiske@web.de>
2017-06-01 23:15:51 +0200

    Fix of JuniorAccountTest
    by
    - typos
    - using AccountTest.assertThrowsWithCause

--- src/test/java/banking/backend/accounts/JuniorAccountTest.java
+++ src/test/java/banking/backend/accounts/JuniorAccountTest.java
@@ -5,6 +5,8 @@ import banking.backend.persons.CustomerTest;
 import banking.backend.persons.Person;
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.InvocationTargetException;
+
 import static org.junit.jupiter.api.Assertions.assertSame;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
@@ -17,51 +19,51 @@ class JuniorAccountTest {
 
     @Test
     public void testOverdraft() {
-        AccountTest.testOverdraft(new CurrentAccount(CustomerTest.getDummyCustomer()), true);
+        AccountTest.testOverdraft(new JuniorAccount(CustomerTest.getDummyCustomer(10, false), CustomerTest.getDummyCustomer()), true);
     }
 
     @Test
     public void testBorrowingInterest() {
-        AccountTest.testBorrowingInterest(new CurrentAccount(CustomerTest.getDummyCustomer()), true);
+        AccountTest.testBorrowingInterest(new JuniorAccount(CustomerTest.getDummyCustomer(10, false), CustomerTest.getDummyCustomer()), true);
     }
 
     @Test
     public void testSavingInterest() {
-        AccountTest.testOverdraft(new CurrentAccount(CustomerTest.getDummyCustomer()), false);
+        AccountTest.testSavingInterest(new JuniorAccount(CustomerTest.getDummyCustomer(10, false), CustomerTest.getDummyCustomer()), false);
     }
 
     @Test
     public void testReceiveInvoice() {
-        AccountTest.testReceiveInvoice(new CurrentAccount(CustomerTest.getDummyCustomer()));
+        AccountTest.testReceiveInvoice(new JuniorAccount(CustomerTest.getDummyCustomer(10, false), CustomerTest.getDummyCustomer()));
     }
 
     @Test
     public void testDeposit() {
-        AccountTest.testDeposit(new CurrentAccount(CustomerTest.getDummyCustomer()));
+        AccountTest.testDeposit(new JuniorAccount(CustomerTest.getDummyCustomer(10, false), CustomerTest.getDummyCustomer()));
     }
 
     @Test
     public void testSendInvoice() throws InsufficientFundsException {
-        AccountTest.testSendInvoice(new CurrentAccount(CustomerTest.getDummyCustomer()));
+        AccountTest.testSendInvoice(new JuniorAccount(CustomerTest.getDummyCustomer(10, false), CustomerTest.getDummyCustomer()));
     }
 
     @Test
     public void testWithdraw() throws InsufficientFundsException {
-        AccountTest.testWithdraw(new CurrentAccount(CustomerTest.getDummyCustomer()));
+        AccountTest.testWithdraw(new JuniorAccount(CustomerTest.getDummyCustomer(10, false), CustomerTest.getDummyCustomer()));
     }
 
     @Test
-    public void testConstructingWithValidLegalGuardian() throws InstantiationException, IllegalAccessException {
+    public void testConstructingWithValidLegalGuardian() throws InstantiationException, IllegalAccessException, NoSuchMethodException, InvocationTargetException {
         Person legalGuardian = CustomerTest.getDummyCustomer(18, false);
         Customer customerOfLegalAgeNoneBusiness = CustomerTest.getDummyCustomer(16, false);
         Customer customerOfLegalAgeBusiness = CustomerTest.getDummyCustomer(16, true);
         Customer customerOfNoneLegalAgeNoneBusiness = CustomerTest.getDummyCustomer(16 - 1, false);
         Customer customerOfNoneLegalAgeBusiness = CustomerTest.getDummyCustomer(16 - 1, true);
 
-        assertThrows(IllegalArgumentException.class, () -> customerOfLegalAgeNoneBusiness.setupAccount(JuniorAccount.class, legalGuardian));
-        assertSame(customerOfLegalAgeBusiness, customerOfNoneLegalAgeNoneBusiness.setupAccount(JuniorAccount.class, legalGuardian).getHolder());
-        assertThrows(IllegalArgumentException.class, () -> customerOfLegalAgeBusiness.setupAccount(JuniorAccount.class, legalGuardian));
-        assertThrows(IllegalArgumentException.class, () -> customerOfNoneLegalAgeBusiness.setupAccount(JuniorAccount.class, legalGuardian));
+        AccountTest.assertThrowsWithCause(IllegalArgumentException.class, () -> customerOfLegalAgeNoneBusiness.setupAccount(JuniorAccount.class, legalGuardian));
+        assertSame(customerOfNoneLegalAgeNoneBusiness, customerOfNoneLegalAgeNoneBusiness.setupAccount(JuniorAccount.class, legalGuardian).getHolder());
+        AccountTest.assertThrowsWithCause(IllegalArgumentException.class, () -> customerOfLegalAgeBusiness.setupAccount(JuniorAccount.class, legalGuardian));
+        AccountTest.assertThrowsWithCause(IllegalArgumentException.class, () -> customerOfNoneLegalAgeBusiness.setupAccount(JuniorAccount.class, legalGuardian));
     }
 
     @Test
@@ -72,15 +74,15 @@ class JuniorAccountTest {
         Customer customerOfNoneLegalAgeNoneBusiness = CustomerTest.getDummyCustomer(16 - 1, false);
         Customer customerOfNoneLegalAgeBusiness = CustomerTest.getDummyCustomer(16 - 1, true);
 
-        assertThrows(IllegalArgumentException.class, () -> customerOfLegalAgeNoneBusiness.setupAccount(JuniorAccount.class, legalGuardian));
-        assertThrows(IllegalArgumentException.class, () -> customerOfNoneLegalAgeNoneBusiness.setupAccount(JuniorAccount.class, legalGuardian));
-        assertThrows(IllegalArgumentException.class, () -> customerOfLegalAgeBusiness.setupAccount(JuniorAccount.class, legalGuardian));
-        assertThrows(IllegalArgumentException.class, () -> customerOfNoneLegalAgeBusiness.setupAccount(JuniorAccount.class, legalGuardian));
+        AccountTest.assertThrowsWithCause(IllegalArgumentException.class, () -> customerOfLegalAgeNoneBusiness.setupAccount(JuniorAccount.class, legalGuardian));
+        AccountTest.assertThrowsWithCause(IllegalArgumentException.class, () -> customerOfNoneLegalAgeNoneBusiness.setupAccount(JuniorAccount.class, legalGuardian));
+        AccountTest.assertThrowsWithCause(IllegalArgumentException.class, () -> customerOfLegalAgeBusiness.setupAccount(JuniorAccount.class, legalGuardian));
+        AccountTest.assertThrowsWithCause(IllegalArgumentException.class, () -> customerOfNoneLegalAgeBusiness.setupAccount(JuniorAccount.class, legalGuardian));
     }
 
     @Test
     public void testConstructingWithoutLegalGuardian() throws IllegalAccessException, InstantiationException {
         Customer customerOfNoneLegalAgeNoneBusiness = CustomerTest.getDummyCustomer(16 - 1, false);
-        assertThrows(RuntimeException.class, () -> customerOfNoneLegalAgeNoneBusiness.setupAccount(JuniorAccount.class));
+        assertThrows(NoSuchMethodException.class, () -> customerOfNoneLegalAgeNoneBusiness.setupAccount(JuniorAccount.class));
     }
 }
